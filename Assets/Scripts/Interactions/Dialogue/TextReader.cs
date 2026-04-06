9c740c8c2abe2e29a80880f9893a7af3f7f46e90
Dillan Victory <113623616+raydillanv@users.noreply.github.com>
2026-04-06 15:15:05 -0500

    Working on dialogue system, starting text reader

---
+++ Assets/Scripts/Interactions/Dialogue/TextReader.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+
+public class TextReader : MonoBehaviour
+{
+    // helpful tutorial: https://www.youtube.com/watch?v=1OOWHB-BOAY
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
