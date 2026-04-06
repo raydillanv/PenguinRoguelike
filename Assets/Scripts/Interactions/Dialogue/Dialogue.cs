ad6bc0e4213cc7c208c63122a0067046ae8168f7
Dillan Victory <113623616+raydillanv@users.noreply.github.com>
2026-04-06 17:29:53 -0500

    Added ability to play random texts from a list on the Dialogue

--- Assets/Scripts/Interactions/Dialogue/Dialogue.cs
+++ Assets/Scripts/Interactions/Dialogue/Dialogue.cs
@@ -8,6 +8,10 @@ public class Dialogue : MonoBehaviour
     public TextAsset textFileToParse;
     public bool Repeatable = false;
 
+    public bool GiveRandomText = false;
+
+    public List<TextAsset> RandomTexts = new List<TextAsset>();
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,22 +19,50 @@ public class Dialogue : MonoBehaviour
         _dialogueController = GameObject.FindGameObjectWithTag("DialogueController").GetComponent<DialogueController>();
         _textReaderRef = GameObject.FindGameObjectWithTag("DialogueController").GetComponent<TextReader>();
         _textReaderRef.LoadText(textFileToParse);
+        if (GiveRandomText)
+        {
+            _textReaderRef.LoadText(GetRandomText());
+        }
     }
 
     public void HandleDialogue()
     {
+        
         _dialogueController.OpenPanel();
         _textReaderRef.PassText();
+
+
         if (_textReaderRef.NoMoreLines)
         {
             
             _dialogueController.ClosePanel();
+
+            if (GiveRandomText && Repeatable)
+            {
+                _textReaderRef.LoadText(GetRandomText());
+                _textReaderRef.RepeatText();
+                return;
+            }
 
             if (Repeatable)
             {
                 _textReaderRef.RepeatText();
             }
         }
+    }
+
+    public TextAsset GetRandomText()
+    {
+        if (RandomTexts.Count != 0){
+            int length = RandomTexts.Count;
+            int choice = Random.Range(0, length);
+            return RandomTexts[choice];
+        } else
+        {
+            print("Random is enabled but there are no text files in your list! Defaulting to original text.");
+            return textFileToParse;
+        }
+        
     }
 
 }
