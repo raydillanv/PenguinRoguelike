dba71a957c8f94334c51eac98cac8257e9a18347
Dillan Victory <113623616+raydillanv@users.noreply.github.com>
2026-04-07 19:07:32 -0500

    Working on stage complete script

---
+++ Assets/Scripts/Interactions/StageComplete.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageComplete : MonoBehaviour, IInteractable
+{
+    private Transform _player;
+    private GameManager _gameManager;
+    
+    public string sceneName;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public bool CanInteract()
+    {
+        return true;
+    }
+
+    public void Interact()
+    {
+        SceneManager.LoadScene(sceneName);
+        _gameManager.VisitStage(sceneName);
+    }
+}
