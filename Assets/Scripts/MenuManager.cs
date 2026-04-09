1b8e8c77e7f69dc2af80a1fce1eb753b7997c385
Robert Lewis <caysonlewis@gmail.com>
2026-04-09 14:26:01 -0500

    updating UI

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,20 +3,28 @@ using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
+    private GameObject pauseMenu;
+
+    private bool isPaused;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        isPaused = false;
+        pauseMenu = GameObject.FindGameObjectWithTag("pause");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            pause();
+        }
     }
 
     public void pause()
     {
+        pauseMenu.SetActive(true);
         Time.timeScale = 0;
     }
     public void resume()
